8170f0bc7f21fcb4bb4e26d5cf44e0760597591a
kaushamb tiwari <kaushambtiwari@gmail.com>
2026-07-22 23:58:37 +0530

    More changes added

--- team-rbac-management/app/lib/auth.ts
+++ team-rbac-management/app/lib/auth.ts
@@ -1,10 +1,11 @@
 import bcrypt from "bcryptjs";
 import { PassThrough } from "stream";
 
-export const hashPassword=(password)=>{
+export const hashPassword= async (password: string): Promise<string>=>{
    return bcrypt.hash(password,12);
 };
 
-export const verifyPassword= async (password: string, hashedPassword: string):Promise<boolean>{
+export const verifyPassword= async (password: string, hashedPassword: string):Promise<boolean>=>{
    return bcrypt.compare(password,hashedPassword)
 }
+
