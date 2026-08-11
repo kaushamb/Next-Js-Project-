3aada9056525f8b6f590e5ac3a2d4c2bd3199113
kaushamb tiwari <kaushambtiwari@gmail.com>
2026-08-11 06:42:26 +0530

    corrected prisma client connection installed dotenv and corrected types installed for bcrypt and json web token created prisma config for deprecated env database url and removed it from  datasource from schema prisma

--- team-rbac-management/app/lib/db.ts
+++ team-rbac-management/app/lib/db.ts
@@ -1,6 +1,18 @@
 import { PrismaClient } from "@prisma/client/extension";
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
-export const prisma= new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma= globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+    log: ['query'],
+  });
 
 //database helper function
 export async function checkDataBaseConnection(): Promise<boolean> {
