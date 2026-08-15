0b8e8c917010e1c287edac03c7db7aebab07d097
kaushamb tiwari <kaushambtiwari@gmail.com>
2026-08-15 20:46:32 +0530

    seed issue fixed , added seeding data for other load some dummy data, added users

--- team-rbac-management/app/lib/db.ts
+++ team-rbac-management/app/lib/db.ts
@@ -1,7 +1,9 @@
-// import { PrismaClient } from "@prisma/client/extension";
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
 
 const connectionString = process.env.DATABASE_URL;
 
