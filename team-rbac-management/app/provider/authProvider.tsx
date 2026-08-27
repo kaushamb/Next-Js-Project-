66ee215608b1106d9b05e97bf348f482085b4650
kaushamb tiwari <kaushambtiwari@gmail.com>
2026-08-27 23:35:42 +0530

    Provider added

--- team-rbac-management/app/provider/authProvider.tsx
+++ team-rbac-management/app/provider/authProvider.tsx
@@ -2,7 +2,7 @@
 import { createContext, useActionState, useState } from "react";
 import { AuthContextType, User } from "../types";
 
-type loginState = {
+type LoginState = {
     success?: boolean,
     user?: User | null;
     error?: string
@@ -11,12 +11,19 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
   // React 19 useActionState for login
-  const [loginState, loginAction, isLoadingPending]= useActionState(,initialState)
+  const [loginState, loginAction, isLoadingPending]= useActionState(
+    async (prevState: LoginState,formData: FormData): Promise<LoginState>=>{
+        const email=formData.get("email") as string;
+        const password = formData.get("password") as string;
+    },
+    {
+        error: undefined, success: undefined, user: undefined
+    } as LoginState)
   return (
     <AuthContext.Provider
       value={{
-        user,
-        login,
+
+        user,        login,
         logout,
         hasPermission,
       }}
